10b7c257114f0aaa914c752e95ba155d8293c19c
Carlos Allende <carlosallendesanz@gmail.com>
2017-05-23 22:40:24 -0700

    book selected action reducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BoooksReducer from './reducer_books'
+import ActiveBookReducer from './reducer_active_book'
 
 const rootReducer = combineReducers({
-  books: BoooksReducer
+  books: BoooksReducer,
+  selectedBook: ActiveBookReducer
 });
 
 export default rootReducer;
